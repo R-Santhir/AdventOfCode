0da24b51e92c0ce40ce3842fc2cf80d8bb52801d
Ryan Santhirarajan <r.santhirarajan@gmail.com>
2015-12-02 12:12:49 -0500

    Day One solutions

---
+++ dayOnePtOne.py
@@ -0,0 +1,5 @@
+from solvers import Solvers
+
+s = Solvers()
+finalFloor = s.moveFloors('test.txt')
+print finalFloor
---
+++ dayOnePtTwo.py
@@ -0,0 +1,5 @@
+from solvers import Solvers
+
+s = Solvers()
+inBasement = s.enteredBasement('test.txt')
+print inBasement
